4d6ee6f5f22b36f02d158f6d45cc2efdb8cf97e7
zhousl <57696015+xieyiincuit@users.noreply.github.com>
2021-08-26 18:39:09 +0800

    仓储

---
+++ src/Services/ESServices/XieyiESLibrary/Provider/ESRepository.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Nest;
+using XieyiESLibrary.Extensions;
+using XieyiESLibrary.Provider.Base;
+
+namespace XieyiESLibrary.Provider
+{
+    public class ESRepository : IESRepository
+    {
+        private readonly IElasticClient _elasticClient;
+        private readonly ILogger<ESRepository> _logger;
+
+        public ESRepository(IESClientProvider clientProvider, ILogger<ESRepository> logger)
+        {
+            _logger = logger;
+            _elasticClient = clientProvider.ElasticClient;
+        }
+
+        public async Task InsertAsync<T>(T entity, string index = "") where T : class
+        {
+            var indexName = index.GetIndex<T>();
+            bool exist = await IndexExistsAsync(indexName);
+            if (!exist)
+            {
+                await ((ElasticClient)_elasticClient).CreateIndexAsync<T>(indexName);
+            }
+
+            var response = await _elasticClient.IndexAsync(entity, x => x.Index(index));
+
+            if (!response.IsValid)
+            {
+                throw new Exception($"add entity fail! :{response.OriginalException.Message}");
+            }
+
+        }
+
+        public async Task InsertRangeAsync<T>(IEnumerable<T> entities, string index = "") where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task BulkIndexAsync<T>(IEnumerable<T> entities, string index = "") where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> IndexExistsAsync(string index)
+        {
+            var result = await _elasticClient.Indices.ExistsAsync(index);
+            return result.Exists;
+        }
+
+        public async Task DeleteIndexAsync(string index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<DeleteByQueryResponse> DeleteByQueryAsync<T>(Expression<Func<T, bool>> expression, string index = "") where T : class, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IUpdateResponse<T>> UpdateAsync<T>(string key, T entity, string index = "") where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<BulkAliasResponse> AddAliasAsync(string index, string alias)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<BulkAliasResponse> AddAliasAsync<T>(string alias) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public BulkAliasResponse RemoveAlias(string index, string alias)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BulkAliasResponse RemoveAlias<T>(string alias) where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Services/ESServices/XieyiESLibrary/Provider/IESRepository.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Nest;
+
+namespace XieyiESLibrary.Provider
+{
+    /// <summary>
+    /// ES CURD Repository
+    /// </summary>
+    public interface IESRepository
+    {
+
+        #region Index Part
+        /// <summary>
+        /// 往Index中新增数据
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entity"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        Task InsertAsync<T>(T entity, string index = "") where T : class;
+
+        /// <summary>
+        /// 往Index中批量新增数据
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entities"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        Task InsertRangeAsync<T>(IEnumerable<T> entities, string index = "") where T : class;
+
+        /// <summary>
+        /// Bulk批量新增
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entities"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        Task BulkIndexAsync<T>(IEnumerable<T> entities, string index = "") where T : class;
+
+        /// <summary>
+        /// 判断索引是否存在
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        Task<bool> IndexExistsAsync(string index);
+
+        /// <summary>
+        /// 删除索引
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        Task DeleteIndexAsync(string index);
+
+        #endregion
+
+        #region Update Part
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="entity"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        Task<IUpdateResponse<T>> UpdateAsync<T>(string key, T entity, string index = "") where T : class;
+
+        #endregion
+
+        #region Delete Part
+
+        Task<DeleteByQueryResponse> DeleteByQueryAsync<T>(Expression<Func<T, bool>> expression,
+            string index = "") where T : class, new();
+
+        #endregion
+
+        #region Alias Part
+
+        /// <summary>
+        ///     添加别名
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="alias"></param>
+        /// <returns></returns>
+        Task<BulkAliasResponse> AddAliasAsync(string index, string alias);
+
+        Task<BulkAliasResponse> AddAliasAsync<T>(string alias) where T : class;
+
+        /// <summary>
+        ///     删除别名
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="alias"></param>
+        /// <returns></returns>
+        BulkAliasResponse RemoveAlias(string index, string alias);
+
+        BulkAliasResponse RemoveAlias<T>(string alias) where T : class;
+
+        #endregion
+
+    }
+}
