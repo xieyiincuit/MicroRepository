6d79a0226643615977484f6fe522d707d10b7f39
zhoushanlin <57696015+xieyiincuit@users.noreply.github.com>
2021-08-26 23:20:16 +0800

    服务注册拓展方法

---
+++ src/Services/ESServices/XieyiESLibrary/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using XieyiESLibrary.Config;
+using XieyiESLibrary.Provider;
+using XieyiESLibrary.Provider.Base;
+
+namespace XieyiESLibrary
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddESServiceInDI(this IServiceCollection services, Action<ESConfig> setupAction)
+        {
+            if (setupAction == null) throw new ArgumentNullException(nameof(setupAction), "调用 ElasticSearch 配置时出错，未传入配置信息。");
+
+            services.Configure(setupAction);
+
+            services.AddSingleton<IESClientProvider, ESClientProvider>();
+            services.AddScoped<IESRepository, ESRepository>();
+        }
+    }
+}
