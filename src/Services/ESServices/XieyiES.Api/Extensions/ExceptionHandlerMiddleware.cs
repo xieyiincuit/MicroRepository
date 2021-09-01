1077d7d5d4ee81e222ea31e924821048007a8401
zhousl <zhoushanlin@sobey.com>
2021-09-01 16:15:11 +0800

    自定义异常组件

---
+++ src/Services/ESServices/XieyiES.Api/Extensions/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Serilog;
+
+namespace XieyiES.Api.Extensions
+{
+    /// <summary>
+    ///     自定义异常中间件
+    /// </summary>
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next, ILogger logger)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                var result = new ErrorResult()
+                {
+                    ErrorCode = StatusCodes.Status500InternalServerError,
+                    ErrorMessage = ex.Message,
+                    ErrorStack = ex.StackTrace
+                };
+                _logger.Error($"exceptionHandlerMiddleware catch a error, please fix it");
+                context.Response.ContentType = "application/json;charset=utf-8";
+                await context.Response.WriteAsync(JsonConvert.SerializeObject(result));
+            }
+        }
+    }
+
+    public class ErrorResult
+    {
+        public int ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+        public string ErrorStack { get; set; }
+    }
+}
---
+++ src/Services/ESServices/XieyiES.Api/Extensions/ExceptionHandlerMiddlewareExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+
+namespace XieyiES.Api.Extensions
+{
+    /// <summary>
+    ///     异常处理中间件
+    /// </summary>
+    public static class ExceptionHandlerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomExceptionHandler(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlerMiddleware>();
+        }
+    }
+}
